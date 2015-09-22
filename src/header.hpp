03da4d8f589682e45f7298e65f5f8f8675e14c10
kaczla <kaczla@wp.pl>
2015-09-22 18:26:49 +0200

    Add file

---
+++ src/header.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <sstream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace std;
+using namespace glm;
