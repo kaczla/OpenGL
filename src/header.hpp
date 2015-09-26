675e0f84fe4b0dc4f2ac0b661d20dab9433a5220
kaczla <kaczla@wp.pl>
2015-09-26 16:36:58 +0200

    Update

--- src/header.hpp
+++ src/header.hpp
@@ -1,6 +1,10 @@
+#ifndef _header_hpp_
+#define _header_hpp_
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 #include <fstream>
 #include <sstream>
 #define GLEW_STATIC
@@ -9,8 +13,15 @@
 #include <SDL2/SDL_opengl.h>
 #define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <SOIL/SOIL.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 using namespace std;
 using namespace glm;
+
+#endif
